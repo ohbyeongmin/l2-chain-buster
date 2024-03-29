d50b2ad30618bc9da871d90767b3faa6478be26f
ohbyeongmin <fbottlem@gmail.com>
2024-03-29 10:22:24 +0900

    del all

--- pkg/optimism/tx_generator.go
+++
@@ -1,7 +0,0 @@
-package optimism
-
-import "github.com/ethereum-optimism/optimism/op-service/txmgr"
-
-type TxManager struct {
-	*txmgr.SimpleTxManager
-}
--- scripts/test.js
+++
@@ -1,6 +0,0 @@
-import compare from 'k6/x/l2-chain-buster';
-
-export default function () {
-  console.log(`${compare.isGreater(2, 1)}, ${compare.comparison_result}`);
-  console.log(`${compare.isGreater(1, 3)}, ${compare.comparison_result}`);
-}
